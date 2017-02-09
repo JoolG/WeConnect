d60c84d76ef84203c28e181eb4aceb2573d1d61f
Jocke <Jocke@IM20135>
2017-02-09 08:03:05 +0100

    Add project files.

---
+++ src/WeConnect/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeConnect.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
